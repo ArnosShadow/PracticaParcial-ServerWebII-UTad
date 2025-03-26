2367d4c908f980fd4951d3b79a8e4b6b4ff3b263
Arnos <fj.jaraba.estevez@gmail.com>
2025-03-26 09:16:23 +0100

    Se ha trasladado el codigo de autentificación, desde los "users" a "auth"

---
+++ controllers/auth.js
@@ -0,0 +1,134 @@
+const { matchedData } = require("express-validator");
+const AuthModel = require("../models/auth");
+const { handleHttpError } = require("../utils/handleError");
+const {JWTSign } =require("../utils/handleJWT");
+const {cifrar, descrifrarComparar} = require("../utils/handlePassword");
+const {comprobarVerificadoEmail} = require("../utils/handleVerificador");
+
+
+const createItem = async(req, res) =>{
+    let descripcion_error = "ERROR: No se ha podido crear el usuario: ";
+    let codigo_error = 400
+    try{
+        const body = matchedData(req);
+        if((typeof body.password) != String){
+            body.password=(body.password).toString();
+        }
+        
+        //Comprobamos si el correo ya fue verificado.
+        if(await comprobarVerificadoEmail(body.email, AuthModel)){
+            codigo_error=409;
+            descripcion_error="ERROR:El email ya está registrado y verificado.";
+            throw err;
+        }
+
+        //ciframos la contraseña
+        body.password=await cifrar(body.password);
+
+        // Se generará un código aleatorio de seis dígitos.
+        const codigoAleatorio = Math.floor(100000 + Math.random() * 900000);
+
+        body.codigoValidacion = codigoAleatorio;
+
+        console.log(codigoAleatorio);
+        const result =await AuthModel.create(body);
+        console.log("Recurso creado: "+result);
+
+        // Generamos un token para pasarselo a nuestro usuario.
+        const token=await JWTSign(result);
+
+        res.status(201).json(
+            {
+                email: result.email,
+                Verificado: result.estadoValidacion,
+                role: result.role,
+                token
+            }
+        );
+    }catch(err){
+        handleHttpError(res,descripcion_error + err, codigo_error);
+    }
+
+}
+
+const validateItem = async (req, res) => {
+    let descripcion_error = "Error en el servidor: ";
+    let codigo_error = 500;
+
+    try {
+        const email = req.body.email;   
+        const code = req.body.code;
+
+        if (!email || !code) {
+            descripcion_error = "Faltan datos obligatorios";
+            codigo_error = 400;
+            throw err;
+        }
+
+        if ((code.toString()).length !== 6) {
+            descripcion_error = "El código debe ser de 6 dígitos numéricos";
+            codigo_error = 400;
+            throw err;
+        }
+
+        const usuario = await AuthModel.findOne({ email });
+
+        if (!usuario) {
+            descripcion_error = "Usuario no encontrado";
+            codigo_error = 404;
+            throw err;
+        }
+
+        if (code !== usuario.codigoValidacion) {
+            descripcion_error = "Código de validación incorrecto";
+            codigo_error = 400;
+            throw err;
+        }
+
+        usuario.estadoValidacion = "Validado";
+        await usuario.save();
+
+        res.status(200).json({ message: 'Usuario valido' });
+
+    } catch (err) {
+        handleHttpError(res, descripcion_error + err.message, codigo_error);
+    }
+};
+
+const loginItem =async (req, res) =>{
+    let descripcion_error = "Error en el login: ";
+    let codigo_error = 400;
+
+    console.log("asdf");
+    try {
+        console.log("asdf");
+        const email= req.body.email;
+        let password = req.body.password;
+        if((typeof password) != String){
+            password=(password).toString();
+        }
+        const result =  await AuthModel.findOne({email:email});
+        console.log(result)
+        if(!descrifrarComparar(password, result.password)){
+
+            descripcion_error = "Contraseña o email incorrecto";
+            codigo_error = 400;
+            throw err;
+        }
+
+        const token=await JWTSign(result);
+
+        res.status(200).json({
+            id: result._id,
+            email: result.email,
+            Verificado: result.estadoValidacion,
+            role: result.role,
+            token
+        });
+    } catch (err) {
+        handleHttpError(res,descripcion_error + err,codigo_error);
+    }
+};
+
+
+module.exports = {createItem,validateItem,loginItem};
---
+++ routes/auth.js
@@ -0,0 +1,22 @@
+const express= require("express");
+const {createItem,validateItem, loginItem} = require("../controllers/auth");
+const { validationResult } = require("../validator/auth");
+const { authMiddleware } = require("../utils/authMiddleware");
+const { loginValidator } = require("../validator/loginValidator");
+const router = express.Router();
+
+
+router.post("/registro", validationResult,createItem);
+router.post("/validacion",authMiddleware ,validateItem);
+router.post("/login",loginValidator,loginItem);
+
+
+//Datos personales
+//router.post("/", usuarioValidator ,usuarioCreacion);
+//Datos de la compañia
+//router.post("/companya", companyaValidator, compayiaCreacion);
+
+
+module.exports = router;
+
+
